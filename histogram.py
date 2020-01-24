dd1a1e73a5d47f4d1ed369faa6b1b20920eae34c
Adam Traub <traubad@vcu.edu>
2020-01-23 19:14:27 -0500

    added histogram, removed mean

---
+++ histogram.py
@@ -0,0 +1,13 @@
+from sys import argv
+import matplotlib.pyplot as plt
+
+values = [int(e) for e in argv[-1].split(",") if e.isdigit()]
+commands = argv[-2].replace("g","").replace("m","")
+if commands.endswith(".py") or not commands:
+    title_string = "Untouched Image Histogram"
+else:
+    title_string = f"Image Histogram after {commands}"
+title_string += f"\nMean: {round(sum([i*v for i,v in enumerate(values)])/sum(values))}"
+plt.title(title_string)
+plt.plot(range(256), values)
+plt.show()
--- main.c
+++ main.c
@@ -6,62 +6,74 @@
 
 #define temp_filename ".temp.bmp"
 
-int strlength(const char* string) //https://stackoverflow.com/questions/25578886/
-{
+
+int strlength(const char* string){ //https://stackoverflow.com/questions/25578886/
     int i;
     for(i=0;string[i]!='\0';i++);
     return i;
 }
 
+size_t join_integers(UINT *num, size_t num_len, char *buf, size_t buf_len) {
+    size_t i;
+    UINT written = strlength(buf);
+
+    for(i = 0; i < num_len; i++) {
+        written += snprintf(buf + written, buf_len - written, (i != 0 ? ",%lu" : "%lu"),
+            *(num + i));
+        if(written == buf_len)
+            break;
+    }
+
+    return written;
+}
+
 void create_files(BMP** in, BMP** out, const char* filename){
-  *in = BMP_ReadFile( filename );
-  *out = BMP_ReadFile( filename );
+    *in = BMP_ReadFile( filename );
+    *out = BMP_ReadFile( filename );
 }
 
 void clear_and_delete(BMP** in, BMP** out){
-  if(*in != *out){ //avoids a seg fault
-    BMP_Free( *out );
-  }
-  BMP_Free( *in );
-  remove(temp_filename);
+    if(*in != *out){ //avoids a seg fault
+      BMP_Free( *out );
+    }
+    BMP_Free( *in );
+    remove(temp_filename);
 }
 
 void apply_left_rotation(BMP** bmp_in, BMP** bmp_out, UINT x, UINT y, UINT height){
     UCHAR val;
     BMP_GetPixelIndex(*bmp_in, height-y-1, x, &val);
     BMP_SetPixelIndex(*bmp_out, x, y, val);
-  }
+}
 
 void apply_right_rotation(BMP** bmp_in, BMP** bmp_out, UINT x, UINT y, UINT height){
     UCHAR val;
     BMP_GetPixelIndex(*bmp_in, y, height-x-1, &val);
     BMP_SetPixelIndex(*bmp_out, x, y, val);
-  }
+}
 
 void apply_mirror_vertical(BMP** bmp_in, BMP** bmp_out, UINT x, UINT y, UINT height){
     UCHAR val;
     BMP_GetPixelIndex(*bmp_in, height-x-1, y, &val);
     BMP_SetPixelIndex(*bmp_out, x, y, val);
-  }
+}
 
 void apply_mirror_horizontal(BMP** bmp_in, BMP** bmp_out, UINT x, UINT y, UINT height){
     UCHAR val;
     BMP_GetPixelIndex(*bmp_in, x, height-y-1, &val);
     BMP_SetPixelIndex(*bmp_out, x, y, val);
-  }
+}
 
 void apply_negative(BMP** bmp_in, BMP**  bmp_out, UINT x, UINT y){
     UCHAR val;
-    /* Get pixel's RGB values */
     BMP_GetPixelIndex(*bmp_in, x, y, &val);
-    /* Invert RGB values */
-    BMP_SetPixelIndex(*bmp_out, x, y, 255 - val);
+    BMP_SetPixelIndex(*bmp_out, x, y, 255 - val);//Invert color value
 }
 
 void adjust_contrast(BMP** bmp_in, BMP** bmp_out, UINT x, UINT y, float contrast){
     UCHAR val;
+
     float factor = (259 * (contrast + 255)) / (255 * (259 - contrast));
-    /* Get pixel's RGB values */
     BMP_GetPixelIndex(*bmp_in, x, y, &val);
 
     val = factor * (val - 128) + 128;
@@ -71,13 +83,12 @@ void adjust_contrast(BMP** bmp_in, BMP** bmp_out, UINT x, UINT y, float contrast
       val = 0;
     }
 
-    /* Invert RGB values */
     BMP_SetPixelIndex(*bmp_out, x, y, val);
 }
 
 void black_and_white(BMP** bmp_in, BMP** bmp_out, UINT x, UINT y){
     UCHAR val;
-    /* Get pixel's RGB values */
+
     BMP_GetPixelIndex(*bmp_in, x, y, &val);
 
     if (val > 255/2){
@@ -85,21 +96,20 @@ void black_and_white(BMP** bmp_in, BMP** bmp_out, UINT x, UINT y){
     } else{
       val = 0;
     }
-
-    /* Invert RGB values */
     BMP_SetPixelIndex(*bmp_out, x, y, val);
 }
 
-int main( int argc, char* argv[] )
-{
+int main( int argc, char* argv[] ){
     BMP*    bmp_in;
     BMP*    bmp_out;
     UINT    width, height;
     UINT    x, y;
+    UCHAR   val;
 
-    if ( strcmp(argv [ 1 ], "--help") == 0 )
-    {
-        printf("\n\nFormat: inFilename, outfilename, commands\n");
+    UINT histogram[256] = {0};
+
+    if ( argc < 3 || strcmp(argv [ 1 ], "--help") == 0 ){
+        printf("\n\nFormat: inFilename, outfilename, commands\n\n");
         printf("Commands include:\n");
         printf("\tl: rotate left\n");
         printf("\tr: rotate right\n");
@@ -107,7 +117,8 @@ int main( int argc, char* argv[] )
         printf("\tv: mirror vertical\n");
         printf("\tb: black and white\n");
         printf("\tn: negative\n");
-        printf("\tc: increase contrast by 10\n\n");
+        printf("\tc: increase contrast by 10\n");
+        printf("\tg: Create a histogram\n\n");
         return 0;
     }
 
@@ -155,7 +166,12 @@ int main( int argc, char* argv[] )
               adjust_contrast(&bmp_in, &bmp_out, x, y, 10);
               break;
 
-            default:
+            case 'g':
+              BMP_GetPixelIndex(bmp_in, x, height-y-1, &val);
+              histogram[val] += 1;
+              break;
+
+            default: //TODO this could be better
               fprintf(stderr, "\nError: '%c' is a bad command\n\n", argv[ 3 ][ i ]);
               clear_and_delete(&bmp_in, &bmp_out);
               return 1;
@@ -169,6 +185,19 @@ int main( int argc, char* argv[] )
           BMP_GetError();
           bmp_in = bmp_out;
           bmp_out = BMP_ReadFile( temp_filename );
+      }
+      if(argv[ 3 ][i] == 'g'){
+        size_t size;
+        char buf[1100] = "python3 histogram.py ";
+        for(int j=0; j< i; j++)
+          buf[strlength(buf)] = argv[3][j];
+        strcat(buf, " ");
+
+        size = join_integers(histogram, 256, buf, 1100);
+        strcat(buf," &");
+
+        system(buf);
+        memset(histogram, 0, 256 * sizeof(histogram[0]));
       }
     }
     bmp_in = bmp_out;
